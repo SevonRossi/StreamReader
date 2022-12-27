7afb1a71fa221092e3412cd4f663ff862177697f
Srossi28 <105078485+Srossi27@users.noreply.github.com>
2022-12-26 21:16:42 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+
+public class CodingChallenge
+{
+    static void Main(string[] args)
+    {
+        String line;
+        int sum;
+        List<int> sumslist = new List<int>();
+
+
+        StreamReader reader = new StreamReader("Sums.txt");
+        StreamWriter writer = new StreamWriter("SumsOutput.txt");
+
+
+        while ((line = reader.ReadLine()) != null)
+        {
+            sum = 0;
+
+            string[] numbers = new string[10];
+            for (int i = 0; i < 10; i++)
+            {
+                numbers[i] = line.Substring(i * 3, 3);
+            }
+
+            foreach (string number in numbers)
+            {
+                sum += int.Parse(number);
+            }
+
+            writer.WriteLine(line + sum.ToString().PadLeft(3, '0'));
+
+            sumslist.Add(sum);
+        }
+
+        int grandTotal = 0;
+        foreach (int i in sumslist)
+        {
+            grandTotal += i;
+        }
+       
+        writer.WriteLine("Grand Total = " + grandTotal);
+    }
+
+}
